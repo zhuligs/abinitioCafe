af5a0931261d806b2e595c9e711d6a3fdb58bd20
Li Zhu(GL) <z@zhuli.name>
2016-11-22 17:26:52 -0500

    add option l

--- xdat2jmol.py
+++ xdat2jmol.py
@@ -4,7 +4,7 @@ import numpy as np
 from optparse import OptionParser
 
 
-def xdat2jmol(npt):
+def xdat2jmol(npt, l):
     xbuff = []
     with open('XDATCAR') as f:
         for line in f:
@@ -30,10 +30,11 @@ def xdat2jmol(npt):
             lat = np.array(xbuff[k + 2: k + 5], float)
             pos = np.array(xbuff[k + 8: k + 8 + nat], float)
             xyz = np.dot(pos, lat)
-            f.write(str(nat) + '\n')
-            f.write('xdat\n')
-            for i in range(len(xyz)):
-                f.write("%2s %15.9f %15.9f %15.9f\n" % (symbs[i], xyz[i][0], xyz[i][1], xyz[i][2]))
+            if i % l == 0 or i == 0:
+                f.write(str(nat) + '\n')
+                f.write('xdat\n')
+                for i in range(len(xyz)):
+                    f.write("%2s %15.9f %15.9f %15.9f\n" % (symbs[i], xyz[i][0], xyz[i][1], xyz[i][2]))
 
         f.close()
     else:
@@ -42,17 +43,20 @@ def xdat2jmol(npt):
             k = 7 + i * (1 + nat)
             pos = np.array(xbuff[k + 1: k + 1 + nat], float)
             xyz = np.dot(pos, lat)
-            f.write(str(nat) + '\n')
-            f.write('xdat\n')
-            for i in range(len(xyz)):
-                f.write("%2s %15.9f %15.9f %15.9f\n" % (symbs[i], xyz[i][0], xyz[i][1], xyz[i][2]))
+            if i % l == 0 or i == 0:
+                f.write(str(nat) + '\n')
+                f.write('xdat\n')
+                for i in range(len(xyz)):
+                    f.write("%2s %15.9f %15.9f %15.9f\n" % (symbs[i], xyz[i][0], xyz[i][1], xyz[i][2]))
         f.close()
 
 
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.set_defaults(is_npt=False)
+    parser.set_defaults(is_npt=False,
+                        l=1)
     parser.add_option("-p", dest="is_npt", action="store_true")
+    parser.add_option("-l", dest="l", type="int")
     (options, args) = parser.parse_args()
-    xdat2jmol(options.is_npt)
+    xdat2jmol(options.is_npt, options.l)
 
