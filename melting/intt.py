51d8dcb8c8aeeded7d50a2898772143455723cc0
Li Zhu(GL) <z@zhuli.name>
2017-12-05 14:27:25 -0500

    dev0

--- melting/intt.py
+++ melting/intt.py
@@ -21,12 +21,18 @@ def twoD_Gaussian( (x, y), amplitude, xo, yo, sigma_x, sigma_y, theta, offset ):
     return g.ravel()
 
 
-x = np.arange(-12.5, 12.5, 0.1)
-y = np.arange(-12.5, 12.5, 0.1)
+x = np.arange(-12.5, 12.5, 0.01)
+y = np.arange(-12.5, 12.5, 0.01)
 x, y = np.meshgrid(x, y)
 
-X = x
-Y = y 
+# X = np.arange(0, 250, 1)
+# Y = np.arange(0, 250, 1)
+xxx, yyy = np.meshgrid(np.linspace(-12.5,12.5,2500), np.linspace(-12.5,12.5,2500))
+X = xxx
+Y = yyy
+print X
+Z = xxx*0.0
+# Y = y 
 zz = x * 0.0
 Zs = []
 for i in range(6):
@@ -59,11 +65,29 @@ datas= [data0, data1, data2, data3,data4, data5]
 norm = plt.cm.colors.Normalize(vmax=3000, vmin=1500)
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-for i in range(6):
-    Z = Zs[i]
-    data = datas[i]
-    ax.plot_surface(X, Y, Z, facecolors=plt.cm.jet(data), shade=False)
+
+ax0 = fig.add_subplot(121)
+ax0.imshow(data0.reshape(2500, 2500), cmap=plt.cm.jet, norm=norm, extent=[-12.5, 12.5, -12.5,12.5])
+Z = data0.reshape(2500, 2500)
+Z2 = data3.reshape(2500, 2500)
+print data0.reshape(2500, 2500)
+ax = fig.add_subplot(122, projection='3d')
+min1 = min(data0)
+min2 = max(data0)
+nn = 16
+
+delt = (min2-min1)/nn
+levs=[]
+for i in range(nn+1):
+    levs.append(min1+i*delt)
+# ax.plot_surface(X, Y, Z, cmap=plt.cm.jet, norm=norm, shade=False)
+cset = ax.contourf(X, Y, Z, zdir='z', levels=levs, offset = 0, norm=norm, cmap=plt.cm.jet)
+cset = ax.contourf(X, Y, Z2, zdir='z', offset = 3, norm=norm, cmap=plt.cm.jet)
+ax.set_zlim3d(-1,5)
+# for i in range(6):
+#     Z = Zs[i]
+#     data = datas[i]
+#     ax.plot_surface(X, Y, Z, facecolors=plt.cm.jet(data0.reshape(250, 250)), shade=False)
 
 
 # plt.subplot(5,1,1)
